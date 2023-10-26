b43a8507125c1977b0903d627e06f44b82aceedf
kelvin <denlinato@gmail.com>
2023-10-26 10:43:05 +0000

    checking

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,67 +1,68 @@
 import { Video } from "iconsax-react";
+import Header from "./Header";
 
 const Hero = () => {
   return (
     <>
-      <section
-        id="home"
-        className="md:bg-hero  bg-hero_mobile bg-no-repeat sm:h-screen h-[45vh] sm:w-screen "
-      >
-        <div className=" h-full  mx-auto max-w-7xl px-6 lg:px-8 pt-20   hidden sm:block ">
-          <div className=" space-y-5">
-            <h1 className=" text-4xl lg:text-[2.6rem]  lg:leading-[2.9rem]  leading-10  text-left text-gray-900 max-w-3xl">
+      <section id="home" className="">
+        <Header />
+        <div className=" h-full  mx-auto  max-w-7xl px-6 lg:px-8 pt-20  md:translate-y-12 md:translate-x-6 hidden sm:block ">
+          <div className=" space-y-7 md:translate-y-10 md:translate-x-10 ">
+            <h1 className=" text-4xl lg:text-[2.6rem]  lg:leading-[2.9rem]  leading-10  text-left text-gray-900 max-w-xl">
               Your Personalized Meal Planning Companion
             </h1>
 
-            <p className="text-[#475467] text-xl">
+            <p className="text-[#475467]">
               Discover personalized nutrition and health support, like never
               before.
             </p>
-            <div className="space-x-2 flex">
+            <div className="space-x-5 flex">
               <button
                 type="button"
-                className="rounded-md text-stone-900 border px-2.5 py-1.5 text-sm  hover:bg-slate-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 flex  space-x-1"
+                className="rounded-md text-stone-900 border px-2.5 py-1.5 md:pl-3 md:pr-3 md:py-2 md:px-2 md:text-lg text-sm    hover:bg-slate-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 flex  space-x-1"
               >
                 <Video size="15" color="black" variant="Bold" />
                 <span>Watch video</span>
               </button>
               <button
                 type="button"
-                className="rounded-md border px-2.5 py-1.5 text-sm  bg-[#1170FF] text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+                className="rounded-md border px-2.5 py-1.5 md:pl-3 md:pr-3 md:py-2 md:px-2 md:text-lg text-sm  bg-[#1170FF] text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
               >
                 Sign up for waitlist
               </button>
             </div>
           </div>
         </div>
-      </section>
-      <div className=" h-full   mx-auto max-w-7xl px-6 lg:px-8  sm:hidden  ">
-        <div className=" space-y-5">
-          <h1 className=" text-4xl lg:text-[2.6rem]   lg:leading-[2.9rem]  leading-10  text-center text-gray-900 max-w-3xl sm:text-left ">
-            Your Personalized Meal Planning Companion
-          </h1>
 
-          <p className="text-[#475467] text-xl text-center sm:text-left">
-            Discover personalized nutrition and health support, like never
-            before.
-          </p>
-          <div className="space-x-2 flex w-full justify-evenly">
-            <button
-              type="button"
-              className="rounded-md text-stone-900 border px-2.5 py-1.5 text-sm  hover:bg-slate-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 flex  space-x-1"
-            >
-              <Video size="15" color="black" variant="Bold" />
-              <span>Watch video</span>
-            </button>
-            <button
-              type="button"
-              className="rounded-md border px-2.5 py-1.5 text-sm  bg-[#1170FF] text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
-            >
-              Sign up for waitlist
-            </button>
+        <div className="gggg h-full   mx-auto max-w-7xl px-6 lg:px-8 translate-y-12 translate-x-6  sm:hidden  ">
+          <div className=" space-y-5">
+            <h1 className=" text-4xl lg:text-[2.6rem]   lg:leading-[2.9rem]  leading-10  text-center text-gray-900 max-w-3xl sm:text-left ">
+              Your Personalized Meal Planning Companion
+            </h1>
+
+            <p className="text-[#475467] text-xl text-center sm:text-left">
+              Discover personalized nutrition and health support, like never
+              before.
+            </p>
+            <div className="space-x-2 flex w-full justify-evenly">
+              <button
+                type="button"
+                className="rounded-md text-stone-900 border px-2.5 py-1.5 text-sm hover:bg-slate-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 flex  space-x-1"
+              >
+                <Video size="15" color="black" variant="Bold" />
+                <span>Watch video</span>
+              </button>
+              <button
+                type="button"
+                className="rounded-md border px-2.5 py-1.5 text-sm bg-[#1170FF] text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+              >
+                Sign up for waitlist
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+        <div className="swoosh"></div>
+      </section>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    backgroundImage: {
-      hero: "url('./assets/hero.png')",
-      hero_mobile: "url('./assets/hero-mobile.png')",
-    },
-    extend: {},
-  },
+ 
   plugins: [],
 };
