0d3372f232c93c4998b3ab1b1dee2e5d1ed55dcd
kelvin <denlinato@gmail.com>
2023-10-26 11:20:25 +0000

    checking

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <>
       <MobileSideBar onOpen={setOpen} open={open} />
-      <div className="mx-auto max-w-7xl px-6 lg:px-8 flex items-center justify-between py-5 bg-white">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 flex items-center justify-between py-5">
         <h1 className=" tracking-widest font-semibold">
           <span className="hidden sm:block">Mydietneeds</span>{" "}
           <span className="sm:hidden">DIETMATE</span>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Pricing from "../components/Pricing";
@@ -7,7 +7,7 @@ import Footer from "../components/Footers";
 const Home = () => {
   return (
     <>
-      <Header />
+
       <Hero />
       <Features />
       <Pricing />
